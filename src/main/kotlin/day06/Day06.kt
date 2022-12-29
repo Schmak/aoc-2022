1385c562c1070359064cad7ec2d14c32a485668d
Evgeny Shmakov <evgeny.shmakov@jetbrains.com>
2022-12-29 08:57:56 +0400

    Day 06 part 2 solution

--- src/main/kotlin/day06/Day06.kt
+++ src/main/kotlin/day06/Day06.kt
@@ -2,16 +2,19 @@ package day06
 
 import utils.readFile
 
-private const val MARKER_SIZE = 4
+fun part1(input: String): Int = findStart(input, 4)
 
-fun part1(input: String): Int =
-    input.windowedSequence(MARKER_SIZE) { it.toSet().size }
-        .takeWhile { it != MARKER_SIZE }
-        .count() + MARKER_SIZE
+fun part2(input: String): Int = findStart(input, 14)
+
+private fun findStart(input: String, markerSize: Int): Int =
+    input.windowedSequence(markerSize) { it.toSet().size }
+        .takeWhile { it != markerSize }
+        .count() + markerSize
 
 fun parseInput(lines: List<String>) = lines.single()
 
 fun main() {
     val input = parseInput(readFile("06"))
     println(part1(input))
+    println(part2(input))
 }
--- src/test/kotlin/day06/Day06KtTest.kt
+++ src/test/kotlin/day06/Day06KtTest.kt
@@ -11,6 +11,12 @@ class Day06KtTest {
         assertThat(actual).isEqualTo(7)
     }
 
+    @Test
+    fun part2() {
+        val actual = part2(input)
+        assertThat(actual).isEqualTo(19)
+    }
+
     companion object {
         private val input = parseInput(readFile("06", type = "test"))
     }
